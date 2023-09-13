22dd8e13e6ab50e00eab2c76a339aa0694c0aef7
sebekx <sebek.x@gmail.com>
2023-09-13 11:08:06 +0200

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -48,6 +48,17 @@ M.general = {
   },
 }
 
+M.copilot = {
+  i = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+      end,
+      "Copilot Accept",
+      {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+    }
+  }
+}
 -- more keybinds!
 
 return M
